5289d4904f57875ae10aa965bd68022fc7306999
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-05-22 15:06:46 -0700

    refactor: http resource api

--- libs/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,14 +1,18 @@
 import { NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, input, numberAttribute } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { validatePassengerStatus } from '../../util-validation';
+import { httpResource } from '@angular/common/http';
+import { initialPassenger, Passenger } from '../../logic-passenger';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-passenger-edit',
   imports: [
     NgIf,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './passenger-edit.component.html'
 })
@@ -22,6 +26,26 @@ export class PassengerEditComponent {
       validatePassengerStatus(['A', 'B', 'C'])
     ]]
   });
+
+  id = input(0, { transform: numberAttribute });
+  passengerResource = httpResource<Passenger>(() => ({
+    url: 'https://demo.angulararchitects.io/api/passenger',
+    params: {
+      id: this.id()
+    }
+  }), {
+    defaultValue: initialPassenger
+  });
+
+  constructor() {
+    effect(() => {
+      if (this.passengerResource.hasValue()) {
+        this.editForm.patchValue(
+          this.passengerResource.value()
+        );
+      }
+    });
+  }
 
   protected save(): void {
     console.log(this.editForm.value);
